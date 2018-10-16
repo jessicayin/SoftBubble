8b3ed004c6826a0cb77f33bec35ca7080d4be947
amcastro-tri <alejandro.castro@tri.global>
2018-10-16 11:25:30 -0400

    Implements calc_node_normals()

--- doall_bubble.m
+++ doall_bubble.m
@@ -2,6 +2,11 @@
 
 [u, K, F] = membrane3d_sparse(p, t);
 
+% Compute normals at the nodes.
+% Normals are the projection of the C0 face normals into the space of C1
+% vector fields.
+[normal, M, Fn] = calc_node_normals(p, t);
 
-writevtkfile('bubble_sol', p, t, u);
+writevtkfile('bubble_def', p, t, u);
+writevtkfile('bubble_normal', p, t, normal);
 
