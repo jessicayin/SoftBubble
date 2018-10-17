55c916af3ef01b99fe7e234402496970ea567252
amcastro-tri <alejandro.castro@tri.global>
2018-10-17 12:20:24 -0400

    Box level set and gradient

--- calc_box_level_set.m
+++ calc_box_level_set.m
@@ -1,16 +1,29 @@
-function phi = calc_box_level_set(lengths, p_BP)
+function [phi, nabla_phi] = calc_box_level_set(lengths, p_BP_list)
 
 half_lengths = lengths/2;
 
-phi = zeros(length(p_BP), 1);
-for side=1:3
-    pp = p_BP(:, side) - half_lengths(side);
-    pm = -p_BP(:, side) - half_lengths(side);
+npoints = size(p_BP_list, 1);
+nabla_phi = zeros(npoints, 3);
+phi = -9999 * ones(npoints, 1);
 
-    p = max(pp, pm);
-    if (side == 1)
-        phi = p;
-    else        
-        phi = max(phi, p);
+for ipoint = 1:npoints
+    p_BP = p_BP_list(ipoint, :)';
+        
+    for idim = 1:3
+        phip = p_BP(idim) - half_lengths(idim);
+        phim = -p_BP(idim) - half_lengths(idim);              
+        
+        if (phi(ipoint) < phip || phi(ipoint) < phim)            
+            nabla_phi(ipoint, :) = 0;
+            if (phip > phim)
+                phi(ipoint) = phip;                
+                nabla_phi(ipoint, idim) = 1;
+            else
+                phi(ipoint) = phim;
+                nabla_phi(ipoint, idim) = -1;
+            end            
+        end      
+                
     end
+    
 end
